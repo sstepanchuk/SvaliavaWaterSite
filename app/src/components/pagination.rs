d924e791880238460f9bc25e4090bbfbd1b77306
Serhii Stepanchuk <stepanchuk.dev@gmail.com>
2025-01-24 21:35:37 +0200

    WIP: fix pagination

--- app/src/components/pagination.rs
+++ app/src/components/pagination.rs
@@ -3,7 +3,7 @@ use leptos::prelude::*;
 #[component]
 pub fn Comp() -> impl IntoView {
     view! {
-        <div class="mt-8 flex justify-center">
+        <div class="flex justify-center">
             <nav class="inline-flex rounded-md shadow">
                 <a href="#" class="px-3 py-2 rounded-l-md border border-gray-300 bg-white text-gray-500 hover:bg-gray-50">"Попередня"</a>
                 <a href="#" class="px-3 py-2 border-t border-b border-gray-300 bg-blue-600 text-white">"1"</a>
